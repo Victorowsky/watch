afe78634845a84e9341dabcd68fc6cdcb7533b41
Victorowsky <Victorowskyy@gmail.com>
2021-03-29 12:04:02 +0200

    comments and styles changed

--- src/comp/AdminPanel/AdminPanel.js
+++ src/comp/AdminPanel/AdminPanel.js
@@ -12,212 +12,214 @@ import RemoveIcon from "@material-ui/icons/Remove";
 import { useRef } from "react";
 
 const AdminPanel = () => {
-  const {
-    twitchUserData,
-    websiteURL,
-    admin,
-    setAdmin,
-    setCurrentVideoLink,
-    socket,
-    setVideoQueue,
-    videoQueue,
-    setMaxDelay,
-    maxDelay,
-    chatRef,
-  } = useContext(DataContext);
-  const [editVideoLink, setEditVideoLink] = useState();
+	const {
+		twitchUserData,
+		websiteURL,
+		admin,
+		setAdmin,
+		setCurrentVideoLink,
+		socket,
+		setVideoQueue,
+		videoQueue,
+		setMaxDelay,
+		maxDelay,
+		chatRef,
+	} = useContext(DataContext);
+	const [editVideoLink, setEditVideoLink] = useState();
 
-  let { twitchStreamer } = useParams();
+	let { twitchStreamer } = useParams();
 
-  if (twitchStreamer === undefined) {
-    twitchStreamer = "victorowsky_";
-  }
-  twitchStreamer = twitchStreamer?.toLowerCase();
-  // DEFAULT VALUE IS MY CHANNEL
+	if (twitchStreamer === undefined) {
+		twitchStreamer = "victorowsky_";
+	}
+	twitchStreamer = twitchStreamer?.toLowerCase();
+	// DEFAULT VALUE IS MY CHANNEL
 
-  ///  HANDLE SET WIDTH ON DELAY INFO SAME AS CHAT
-  const delayInfoRef = useRef(null);
+	///  HANDLE SET WIDTH ON DELAY INFO SAME AS CHAT
+	const delayInfoRef = useRef(null);
 
-  useEffect(() => {
-    const handleDelayInfoSetWidth = () => {
-      delayInfoRef.current.style.width = chatRef?.current?.offsetWidth + "px";
-    };
-    delayInfoRef.current.style.width = chatRef?.current?.offsetWidth + "px";
-    window.addEventListener("resize", handleDelayInfoSetWidth);
+	useEffect(() => {
+		const handleDelayInfoSetWidth = () => {
+			delayInfoRef.current.style.width = chatRef?.current?.offsetWidth + "px";
+		};
+		delayInfoRef.current.style.width = chatRef?.current?.offsetWidth + "px";
+		window.addEventListener("resize", handleDelayInfoSetWidth);
 
-    return () => {
-      window.removeEventListener("resize", handleDelayInfoSetWidth);
-    };
-  }, [chatRef]);
+		return () => {
+			window.removeEventListener("resize", handleDelayInfoSetWidth);
+		};
+	}, [chatRef]);
 
-  useEffect(() => {
-    if (twitchUserData) {
-      if (twitchUserData.login.toLowerCase() === twitchStreamer.toLowerCase()) {
-        if (!admin) {
-          setAdmin(true);
-        }
-      }
-      return () => {
-        if (admin) {
-          setAdmin(false);
-        }
-      };
-    }
-  }, [twitchUserData, setAdmin, twitchStreamer, admin]);
+	useEffect(() => {
+		if (twitchUserData) {
+			if (twitchUserData.login.toLowerCase() === twitchStreamer.toLowerCase()) {
+				if (!admin) {
+					setAdmin(true);
+				}
+			}
+			return () => {
+				if (admin) {
+					setAdmin(false);
+				}
+			};
+		}
+	}, [twitchUserData, setAdmin, twitchStreamer, admin]);
 
-  useEffect(() => {
-    if (admin) {
-      socket.emit("adminQueueUpdate", {
-        videoQueue,
-        currentRoom: twitchStreamer,
-      });
+	useEffect(() => {
+		if (admin) {
+			socket.emit("adminQueueUpdate", {
+				videoQueue,
+				currentRoom: twitchStreamer,
+			});
 
-      return () => {
-        socket.removeAllListeners("adminQueueUpdate");
-      };
-    }
-  }, [videoQueue, socket, twitchStreamer, admin]);
+			return () => {
+				socket.removeAllListeners("adminQueueUpdate");
+			};
+		}
+	}, [videoQueue, socket, twitchStreamer, admin]);
 
-  const handleAddVideoToQueue = () => {
-    if (editVideoLink) {
-      setVideoQueue((prev) => [...prev, editVideoLink]);
-      setEditVideoLink("");
-    }
-  };
+	const handleAddVideoToQueue = () => {
+		if (editVideoLink) {
+			setVideoQueue((prev) => [...prev, editVideoLink]);
+			setEditVideoLink("");
+		}
+	};
 
-  const handleAddVideo = () => {
-    if (editVideoLink) {
-      setCurrentVideoLink(editVideoLink);
-      setEditVideoLink("");
-    }
-  };
+	const handleAddVideo = () => {
+		if (editVideoLink) {
+			setCurrentVideoLink(editVideoLink);
+			setEditVideoLink("");
+		}
+	};
 
-  const handleChangeMaxDelay = (type) => {
-    if (type === "increment") {
-      setMaxDelay((prev) => prev + 1);
-    } else if (type === "decrement") {
-      setMaxDelay((prev) => {
-        if (prev > 2) {
-          return prev + -1;
-        } else {
-          return prev;
-        }
-      });
-    }
-  };
+	const handleChangeMaxDelay = (type) => {
+		if (type === "increment") {
+			setMaxDelay((prev) => prev + 1);
+		} else if (type === "decrement") {
+			setMaxDelay((prev) => {
+				if (prev > 2) {
+					return prev + -1;
+				} else {
+					return prev;
+				}
+			});
+		}
+	};
 
-  const handleAdminCheckQueue = () => {
-    if (admin) {
-      if (videoQueue) {
-        setCurrentVideoLink(videoQueue[0]);
-        setVideoQueue((prev) => prev.slice(1));
-      }
-    }
-  };
+	const handleAdminCheckQueue = () => {
+		if (admin) {
+			if (videoQueue) {
+				setCurrentVideoLink(videoQueue[0]);
+				setVideoQueue((prev) => prev.slice(1));
+			}
+		}
+	};
 
-  const handleTwitchLogin = () => {
-    window.location.href = `${websiteURL}/auth/twitch`; //DECLARED IN APP
-  };
+	const handleTwitchLogin = () => {
+		window.location.href = `${websiteURL}/auth/twitch`; //DECLARED IN APP
+	};
 
-  const handleLogout = () => {
-    window.location.href = `${websiteURL}/twitch/logout`;
-  };
+	const handleLogout = () => {
+		window.location.href = `${websiteURL}/twitch/logout`;
+	};
 
-  return (
-    <>
-      {admin ? (
-        <>
-          {/* ADDING VIDEO PANEL */}
-          <div className="adminPanel">
-            <form>
-              <input
-                type="text"
-                value={editVideoLink}
-                placeholder={"VIDEO URL"}
-                onChange={(e) => {
-                  if (admin) {
-                    setEditVideoLink(e.target.value);
-                  }
-                }}
-              />
+	return (
+		<>
+			{admin ? (
+				// ADMIN PANEL
+				<>
+					{/* ADDING VIDEO PANEL */}
+					<div className="adminPanel">
+						<form>
+							<input
+								type="text"
+								value={editVideoLink}
+								placeholder={"VIDEO URL"}
+								onChange={(e) => {
+									if (admin) {
+										setEditVideoLink(e.target.value);
+									}
+								}}
+							/>
 
-              <button
-                style={{ display: "none" }}
-                onClick={(e) => {
-                  e.preventDefault();
-                  handleAddVideo();
-                }}
-                type="submit"
-              ></button>
-              <div className="optionButtons">
-                <QueueButton text={"PLAY NOW"} onClick={handleAddVideo} />
-                <QueueButton
-                  text={"ADD TO QUEUE"}
-                  onClick={handleAddVideoToQueue}
-                />
-                <QueueButton text={"SKIP"} onClick={handleAdminCheckQueue} />
-                <Button2 text={"LOGOUT"} onClick={handleLogout} />
-              </div>
-            </form>
-            {twitchUserData && (
-              <div className="accountInfo">
-                <div className="img">
-                  <img src={twitchUserData.image} alt="twitchImage" srcset="" />
-                </div>
-                {twitchUserData.login}
-              </div>
-            )}
-          </div>
-          <Queue />
-        </>
-      ) : (
-        <div className="delayInfoContainer">
-          <Queue />
-          <div className="delay" ref={delayInfoRef}>
-            <span className="delayInfo">Max Delay: {maxDelay} seconds</span>
-            <div className="delayManage">
-              <div
-                className="delayManageOptionDecrement"
-                onClick={() => handleChangeMaxDelay("decrement")}
-              >
-                <RemoveIcon />
-              </div>
-              <div
-                className="delayManageOptionIncrement"
-                onClick={() => handleChangeMaxDelay("increment")}
-              >
-                <AddIcon />
-              </div>
-            </div>
+							<button
+								style={{ display: "none" }}
+								onClick={(e) => {
+									e.preventDefault();
+									handleAddVideo();
+								}}
+								type="submit"
+							></button>
+							<div className="optionButtons">
+								<QueueButton text={"PLAY NOW"} onClick={handleAddVideo} />
+								<QueueButton
+									text={"ADD TO QUEUE"}
+									onClick={handleAddVideoToQueue}
+								/>
+								<QueueButton text={"SKIP"} onClick={handleAdminCheckQueue} />
+								<Button2 text={"LOGOUT"} onClick={handleLogout} />
+							</div>
+						</form>
+						{twitchUserData && (
+							<div className="accountInfo">
+								<div className="img">
+									<img src={twitchUserData.image} alt="twitchImage" srcset="" />
+								</div>
+								{twitchUserData.login}
+							</div>
+						)}
+					</div>
+					<Queue />
+				</>
+			) : (
+				// IS NOT ADMIN
+				<div className="delayInfoContainer">
+					<Queue />
+					<div className="delay" ref={delayInfoRef}>
+						<span className="delayInfo">Max Delay: {maxDelay} seconds</span>
+						<div className="delayManage">
+							<div
+								className="delayManageOptionDecrement"
+								onClick={() => handleChangeMaxDelay("decrement")}
+							>
+								<RemoveIcon />
+							</div>
+							<div
+								className="delayManageOptionIncrement"
+								onClick={() => handleChangeMaxDelay("increment")}
+							>
+								<AddIcon />
+							</div>
+						</div>
 
-            {twitchUserData ? (
-              <div className="accountInfo">
-                <a
-                  href={`${websiteURL}/#/${twitchUserData.login.toLowerCase()}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <div className="img">
-                    <img src={twitchUserData.image} alt="Profile" />
-                  </div>
-                  {twitchUserData.login}
-                </a>
-              </div>
-            ) : (
-              <>
-                <div className="twitchLoginButton">
-                  <Button2
-                    text={"LOGIN WITH TWITCH"}
-                    onClick={handleTwitchLogin}
-                  />
-                </div>
-              </>
-            )}
-          </div>
-        </div>
-      )}
-    </>
-  );
+						{twitchUserData ? (
+							<div className="accountInfo">
+								<a
+									href={`${websiteURL}/#/${twitchUserData.login.toLowerCase()}`}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<div className="img">
+										<img src={twitchUserData.image} alt="Profile" />
+									</div>
+									{twitchUserData.login}
+								</a>
+							</div>
+						) : (
+							<>
+								<div className="twitchLoginButton">
+									<Button2
+										text={"LOGIN WITH TWITCH"}
+										onClick={handleTwitchLogin}
+									/>
+								</div>
+							</>
+						)}
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default AdminPanel;
--- src/comp/AdminPanel/QueueButton.js
+++ src/comp/AdminPanel/QueueButton.js
@@ -3,31 +3,31 @@ import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      margin: theme.spacing(1),
-      borderColor: "white",
-      color: "white",
-      transition: "0.3s",
-      "&:hover": {
-        boxShadow: "0 0 10px white",
-      },
-    },
-  },
+	root: {
+		"& > *": {
+			margin: theme.spacing(1),
+			borderColor: "white",
+			color: "white",
+			transition: "0.3s",
+			"&:hover": {
+				boxShadow: "0 0 5px white",
+			},
+		},
+	},
 }));
 
 export default function OutlinedButtons({ text, onClick, style }) {
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    <div className={classes.root}>
-      <Button
-        onClick={onClick && onClick}
-        style={style && style}
-        variant="outlined"
-      >
-        {text ? text : "Enter text"}
-      </Button>
-    </div>
-  );
+	return (
+		<div className={classes.root}>
+			<Button
+				onClick={onClick && onClick}
+				style={style && style}
+				variant="outlined"
+			>
+				{text ? text : "Enter text"}
+			</Button>
+		</div>
+	);
 }
